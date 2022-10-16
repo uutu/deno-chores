69a9e511b3ed9b934a137552cf4da2dc9ace5d8a
Juuso-Petteri Kuisma <juuso.kuisma@gmail.com>
2022-10-16 15:29:42 +0300

    Add dependencies file

---
+++ app/deps.js
@@ -0,0 +1,9 @@
+export { configure, renderFile } from "https://deno.land/x/eta@v1.12.3/mod.ts";
+
+export {
+    Application,
+    Router,
+    send,
+} from "https://deno.land/x/oak@v10.6.0/mod.ts";
+
+export { Pool } from "https://deno.land/x/postgres@v0.16.1/mod.ts";
