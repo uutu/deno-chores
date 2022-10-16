425a2a17f284af987fa25c830aa4647b3b94d5d2
Juuso-Petteri Kuisma <juuso.kuisma@gmail.com>
2022-10-16 16:26:28 +0300

    Comment out imports

--- app/app.js
+++ app/app.js
@@ -1,13 +1,13 @@
 import { Application } from "./deps.js";
-import { errorMiddleware } from "./middlewares/errorMiddleware.js";
+//import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 import { renderMiddleware } from "./middlewares/renderMiddleware.js";
-import { serveStaticMiddleware } from "./middlewares/"
+import { serveStaticMiddleware } from "./middlewares/serveStaticMiddleware.js";
 
 import { router } from "./routes/routes.js";
 
 const app = new Application();
 
-app.use(errorMiddleware);
+//app.use(errorMiddleware);
 app.use(renderMiddleware);
 app.use(router.routes());
 
---
+++ app/middlewares/errorMiddleware.js
@@ -0,0 +1,5 @@
+const errorMiddleware = async () => {
+
+};
+
+export { errorMiddleware };
