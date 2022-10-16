9b1bc52f8490da2fddf12c10116754543594a896
Juuso-Petteri Kuisma <juuso.kuisma@gmail.com>
2022-10-16 15:17:36 +0300

    Add DB schema

--- flyway/sql/V1___initial_schema.sql
+++ flyway/sql/V1___initial_schema.sql
@@ -1,10 +1,29 @@
-CREATE TABLE names (
+CREATE TABLE users (
   id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL
+  name varchar(255) NOT NULL,
+  address varchar(255) NOT NULL,
+  email varchar(255) NOT NULL UNIQUE,
+  chorecoins integer NOT NULL DEFAULT 0,
+  password varchar(60) NOT NULL
 );
 
-CREATE TABLE addresses (
+CREATE UNIQUE INDEX ON users((lower(email)));
+
+CREATE TABLE chores (
   id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL,
-  address TEXT NOT NULL
+  user_id integer NOT NULL,
+  title varchar(255) NOT NULL,
+  description TEXT NOT NULL,
+  chorecoins integer NOT NULL DEFAULT 0,
+  due_date TIMESTAMP WITH TIME ZONE,
+  CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE chore_assignments (
+  user_id integer NOT NULL,
+  chore_id integer NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE,
+  completed_at TIMESTAMP WITH TIME ZONE,
+  CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id),
+  CONSTRAINT fk_chore_id FOREIGN KEY (chore_id) REFERENCES chores(id)
 );
