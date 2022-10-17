6607d25f38b64c6d46236d0781e1aae86ee2e8cb
Juuso-Petteri Kuisma <juuso.kuisma@gmail.com>
2022-10-17 14:50:49 +0300

    Add Validasaur library and add validation to chores

--- app/deps.js
+++ app/deps.js
@@ -9,3 +9,4 @@ export {
 export { Pool } from "https://deno.land/x/postgres@v0.16.1/mod.ts";
 export { Session } from "https://deno.land/x/oak_sessions@v3.5.1/mod.ts";
 export * as bcrypt from "https://deno.land/x/bcrypt@v0.4.0/mod.ts";
+export * as validasaur from "https://deno.land/x/validasaur@v0.15.0/mod.ts";
--- app/routes/controllers/choreController.js
+++ app/routes/controllers/choreController.js
@@ -1,20 +1,48 @@
 import { executeQuery } from "../../database/database.js";
 import { renderFile } from "../../deps.js";
 import * as choreService from "../../services/choreService.js";
+import { validasaur } from "../../deps.js";
 
-const addChore = async ({ request, response, user }) => {
+const choreValidationRules = {
+    title: [validasaur.required, validasaur.minLength(1)],
+    description: [validasaur.required, validasaur.minLength(1)],
+    chorecoins: [validasaur.required, validasaur.isNumeric],
+    due_date: [validasaur.required, validasaur.isDate],
+};
+
+const getChoreData = async (request) => {
     const body = request.body({ type: "form" });
     const params = await body.value;
+    return {
+        title: params.get("title"),
+        description: params.get("description"),
+        chorecoins: params.get("chorecoins"),
+        due_date: params.get("due_date"),
+    };
+};
 
-    await choreService.addChore(
-        user.id,
-        params.get("title"),
-        params.get("description"),
-        params.get("chorecoins"),
-        params.get("due_date"),
+const addChore = async ({ request, response, render, user }) => {
+    const choreData = await getChoreData(request);
+    
+    const [passes, errors] = await validasaur.validate(
+        choreData,
+        choreValidationRules,
     );
 
-    response.redirect("/chores");
+    if (!passes) {
+        console.log(errors);
+        choreData.validationErrors = errors;
+        render("chores.eta", choreData);
+    } else {
+        await choreService.addChore(
+            user.id,
+            params.get("title"),
+            params.get("description"),
+            params.get("chorecoins"),
+            params.get("due_date"),
+        );
+        response.redirect("/chores");
+    }  
 };
 
 const claimChore = async ({ params, response, user }) => {
