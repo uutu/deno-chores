9b9b4561363742bd78e90e32f8d4aa3e96ea20ee
Juuso-Petteri Kuisma <juuso.kuisma@gmail.com>
2022-10-17 13:05:00 +0300

    Add auth middleware to app

--- app/deps.js
+++ app/deps.js
@@ -7,3 +7,4 @@ export {
 } from "https://deno.land/x/oak@v10.6.0/mod.ts";
 
 export { Pool } from "https://deno.land/x/postgres@v0.16.1/mod.ts";
+export { Session } from "https://deno.land/x/oak_sessions@v3.5.1/mod.ts";
