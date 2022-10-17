a101b7e4aa05fc185ae5eb813a6e5c722ba3cc62
Juuso-Petteri Kuisma <juuso.kuisma@gmail.com>
2022-10-17 14:29:36 +0300

    Add user to rendered object

--- app/middlewares/renderMiddleware.js
+++ app/middlewares/renderMiddleware.js
@@ -6,6 +6,14 @@ configure({
 
 const renderMiddleware = async (context, next) => {
     context.render = async (file, data) => {
+        if (!data) {
+            data = {};
+        }
+
+        if (context.user) {
+            data.user = context.user;
+        }
+        
         context.response.headers.set("Content-Type", "text/html; charset=utf-8");
         context.response.body = await renderFile(file, data);
     };
