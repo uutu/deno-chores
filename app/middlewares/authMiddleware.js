c058e0aa0902e2e73ab665d8e2b7f325ebed977a
Juuso-Petteri Kuisma <juuso.kuisma@gmail.com>
2022-10-17 14:42:20 +0300

    Modify authMiddleware to only allow registered users to view statistics

--- app/middlewares/authMiddleware.js
+++ app/middlewares/authMiddleware.js
@@ -1,4 +1,4 @@
-const restrictedPaths = ["/chores"];
+const restrictedPaths = ["/chores", "/statistics"];
 
 const authMiddleware = async (context, next) => {
     const user = await context.state.session.get("user");
