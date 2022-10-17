60c2740154ccb86f4f0cc7b94b07e3fc99c8bf27
Juuso-Petteri Kuisma <juuso.kuisma@gmail.com>
2022-10-17 13:22:58 +0300

    Map routes to registration controller

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,6 +1,7 @@
 import { Router } from "https://deno.land/x/oak@v10.6.0/mod.ts";
 import * as mainController from "./controllers/mainController.js";
 import * as choreController from "./controllers/choreController.js";
+import * as registrationController from "./controllers/registrationController.js";
 
 const router = new Router();
 
@@ -10,5 +11,8 @@ router.get("/chores", choreController.listChores);
 router.post("/chores", choreController.addChore);
 router.post("/chores/:id/claim", choreController.claimChore);
 router.post("/chores/:id/complete", choreController.completeChore);
+
+router.get("/auth/register", registrationController.showRegistrationForm);
+router.post("/auth/register", registrationController.registerUser);
 
 export { router };
